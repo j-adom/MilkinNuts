be09d10bb7c5df8fc6b1b7aec51b07b579ef4657
Jon Finley <lilfin19@gmail.com>
2020-01-11 13:02:58 -0600

    files

---
+++ milkinnuts/client/src/components/delivery/Delivery.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+
+// import Navbar from "../navbar/Navbar.js";
+
+// import "./MainContent.css";
+// import "./Dashboard.css";
+
+// import { connect } from "react-redux";
+
+// import Modal from "./Modal/Modal";
+
+class Delivery extends Component {
+    render(){
+
+return (
+
+
+    <div>
+        {/* <Navbar/> */}
+<h1>hello World</h1>
+
+
+
+
+
+    </div>
+)       
+
+
+    }
+}
+
+export default Delivery;
---
+++ milkinnuts/client/src/components/home/Home.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+
+import Navbar from "../navbar/Navbar.js";
+
+// import "./MainContent.css";
+// import "./Dashboard.css";
+
+// import { connect } from "react-redux";
+
+// import Modal from "./Modal/Modal";
+
+class Home extends Component {
+    render(){
+
+return (
+
+
+    <div>
+        <Navbar/>
+<h1>hello World</h1>
+
+
+
+
+
+    </div>
+)       
+
+
+    }
+}
+
+export default Home;
+
